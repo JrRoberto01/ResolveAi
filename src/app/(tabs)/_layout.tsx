7989dcd2ee67c375b516f8fcf47220b4a7965dc2
JrRoberto01 <contatorj.roberto@gmail.com>
2026-03-15 16:44:00 -0300

    Add main pages & Navigation base

---
+++ src/app/(tabs)/_layout.tsx
@@ -0,0 +1,46 @@
+import colors from '@/style/colors';
+import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
+import { Tabs } from "expo-router";
+
+export default function TabLayout() {
+    return (
+        <Tabs>
+            <Tabs.Screen name="index"
+                options={{
+                    title: 'Feed',
+                    tabBarActiveTintColor: `${colors.darkBlue}`,
+                    tabBarInactiveTintColor: `${colors.darkGrey}`,
+                    headerShown: false,
+                    tabBarIcon: () => <FontAwesome5 name="home" size={24} color={colors.darkGrey} />,
+                }}
+            />
+            <Tabs.Screen name="ranking"
+                options={{
+                    title: 'Ranking',
+                    tabBarActiveTintColor: `${colors.darkBlue}`,
+                    tabBarInactiveTintColor: `${colors.darkGrey}`,
+                    headerShown: false,
+                    tabBarIcon: () => <FontAwesome5 name="trophy" size={24} color={colors.darkGrey} />,
+                }}
+            />
+            <Tabs.Screen name="favorites"
+                options={{
+                    title: 'Favoritos',
+                    tabBarActiveTintColor: `${colors.darkBlue}`,
+                    tabBarInactiveTintColor: `${colors.darkGrey}`,
+                    headerShown: false,
+                    tabBarIcon: () => <FontAwesome5 name="star" size={24} color={colors.darkGrey} />,
+                }}
+            />
+            <Tabs.Screen name="profile"
+                options={{
+                    title: 'Perfil',
+                    tabBarActiveTintColor: `${colors.darkBlue}`,
+                    tabBarInactiveTintColor: `${colors.darkGrey}`,
+                    headerShown: false,
+                    tabBarIcon: () => <FontAwesome5 name="user" size={24} color={colors.darkGrey} />,
+                }}
+            />
+        </Tabs>
+    )
+}
---
+++ src/style/colors.ts
@@ -0,0 +1,7 @@
+export default{
+    darkBlue: '#1C50B8',
+    lightBlue: '#99B6F0',
+    darkGrey: '#D6D3D1',
+    white: '#FFFFFF',
+    black: '#323232'
+}
